1c8f44bf90acaa544638b1ae5edc8823c3ff8f77
mrmindstormer <mindstorm1@online.de>
2025-06-12 20:22:33 +0200

    Final

--- script/slide-in-text.js
+++ script/slide-in-text.js
@@ -1,9 +1,10 @@
 window.addEventListener('scroll', function() {
       const infoBox = document.getElementById('infoBox');
-      const triggerPoint = 850; // Pixel, ab wann der Text erscheinen soll
+      const triggerPoint = window.innerHeight * 0.6;
       if (window.scrollY > triggerPoint) {
         infoBox.classList.add('visible');
       } else {
         infoBox.classList.remove('visible');
       }
     });
+    //ermöglich das Einblenden desr Info-Box beim Scrollen
---
+++ script/wishlist.js
@@ -0,0 +1,7 @@
+document.getElementById('wishlistBtn').onclick = function() {
+    document.getElementById('wishlistPanel').style.display = 'block';
+};
+document.getElementById('cartBtn').onclick = function() {
+    document.getElementById('cartPanel').style.display = 'block';
+};
+// Ermöglicht das Anzeigen der Merkliste und des Warenkorbs beim Klicken auf die entsprechenden Buttons
